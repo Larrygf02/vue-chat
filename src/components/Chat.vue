bf7b89d879bd8ca68c2ebf709dd11697ec7a8c76
larrygf02 <raulgf_02@hotmail.com>
2020-05-22 21:18:55 -0500

    Init commit chat

---
+++ src/components/Chat.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Chat</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Chat from './components/Chat';
+Vue.use(Router)
+
+const routes = [
+    { path: '/chat', component: Chat }
+]
+
